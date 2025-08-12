fa908621e456e3d832bb717203164be164223c0d
Glebn5 <glebnowikoff@yandex.ru>
2025-08-12 19:32:51 +0300

    add post_view with navigations

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView
-
-# def home_page(request):
-#     context = {
-#         'posts': Post.objects.all(),
-#         }
-#     return render(request, 'blog/home_page.html', context=context)
-
+from django.views.generic.detail import DetailView
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home_page.html'
     context_object_name = 'posts'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post.html'
+    slug_url_kwarg = 'post_slug'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        obj = self.object
+        context['prev_post'] = Post.objects.filter(id__lt=obj.id).last()
+        context['next_post'] = Post.objects.filter(id__gt=obj.id).first()
+        return context
