c46a27ca03c1c54b973e8cc22d61af20fedd71e3
Glebn5 <glebnowikoff@yandex.ru>
2025-08-13 16:42:41 +0300

    add urls for views

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import *
 
 
 urlpatterns = [
     path('home/', PostListView.as_view(), name = 'home_page'),
     path('post/<slug:post_slug>/', PostDetailView.as_view(), name = 'post_page'),
+    path('post_create/', PostCreateView.as_view(), name = 'create_a_post'),
+    path('post_update/<slug:post_slug>/', PostUpdateView.as_view(), name = 'post_update'),
+    path('post_delete/<slug:post_slug>/', PostDeleteView.as_view(), name = 'post_delete'),
 ]
