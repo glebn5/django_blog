be75fcdcb6b390b664016fc1727de2418bcf5ac4
Glebn5 <glebnowikoff@yandex.ru>
2025-08-13 16:43:31 +0300

    add create, update, delete views; add loginrequiredmixin

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 from .models import Post
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import *
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PostListView(ListView):
     model = Post
@@ -19,3 +23,25 @@ class PostDetailView(DetailView):
         context['prev_post'] = Post.objects.filter(id__lt=obj.id).first()
         context['next_post'] = Post.objects.filter(id__gt=obj.id).last()
         return context
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    form_class = CreatePostForm
+    template_name = 'blog/post_create.html'
+
+    def get_success_url(self, **kwargs):
+        return reverse('post_page', kwargs={'post_slug': self.object.slug})
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_update.html'
+    slug_url_kwarg = 'post_slug'
+    
+    def get_success_url(self, **kwargs):
+        return reverse('post_page', kwargs={'post_slug': self.object.slug})
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('home_page')
+    template_name = 'blog/post_delete.html'
+    slug_url_kwarg = 'post_slug'
