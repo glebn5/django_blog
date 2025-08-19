913ea155146f9e85ab3b78de48a0123e5d6f9a52
Glebn5 <glebnowikoff@yandex.ru>
2025-08-19 11:42:59 +0300

    add users/admin.py

--- myblog/users/admin.py
+++ myblog/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import RegisterCustomUserForm, ChangeCustomUserForm
+from .models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = RegisterCustomUserForm
+    form = ChangeCustomUserForm
+    model = CustomUser
+    list_display = ['username', 'email']
+    list_display_links = ['username']
+    list_filter = ['username']
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
