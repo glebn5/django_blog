da8aadbe8f1694c12c98654033019f186b9ef866
Glebn5 <glebnowikoff@yandex.ru>
2025-08-19 17:05:28 +0300

    add reg url

--- myblog/users/urls.py
+++ myblog/users/urls.py
@@ -6,5 +6,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('user_login/', LoginUser.as_view(), name='user_login'),
-    path('user_logout/', LoginView.as_view(), name = 'user_logout')
+    path('user_logout/', LoginView.as_view(), name = 'user_logout'),
+    path('user_reg/', RegisterUser.as_view(), name = 'user_register')
 ]
