8a51dc23fbb6e54de4c4d9ca6500a29ec3b79eec
Glebn5 <glebnowikoff@yandex.ru>
2025-08-19 16:44:08 +0300

    refactor home page url

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('home/', PostListView.as_view(), name = 'home_page'),
+    path('', PostListView.as_view(), name = 'home_page'),
     path('post/<slug:post_slug>/', PostDetailView.as_view(), name = 'post_page'),
     path('post_create/', PostCreateView.as_view(), name = 'create_a_post'),
     path('post_update/<slug:post_slug>/', PostUpdateView.as_view(), name = 'post_update'),
