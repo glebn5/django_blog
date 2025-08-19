78121332e2b5acc63886c73164788cb3e5190039
Glebn5 <glebnowikoff@yandex.ru>
2025-08-19 16:43:36 +0300

    add field aithor in admin

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -4,7 +4,7 @@ from .models import Post
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'content_for_admin', 'created_at']
+    list_display = ['title', 'content_for_admin', 'author','created_at']
     list_display_links = ['title']
     list_filter = ['title']
     prepopulated_fields = {"slug": ["title"]}
